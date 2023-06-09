2108ed34460658c36caf80aed64a4f43506bd90e
djtrueway <saintaimepeterson@gmail.com>
2023-06-09 15:08:28 -0400

    add loading

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,35 @@ import store from "../store/index";
 import "../styles/globals.css"
 import Layout from "../compoments/layouts/Layout"
 
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#f26522",
+  className: "bar-of-progress",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
+
+function MyApp({ Component, pageProps }) {
+  
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+      <Layout>
+        <Container maxWidth="xl">
+          <Component {...pageProps} />
+        </Container>
+      </Layout>
+      </ThemeProvider>
+    </Provider>
+  );
+}
+
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
